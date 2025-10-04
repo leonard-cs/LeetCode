4027a0f397c09311ba2e25650c0c8b2132d19100
Leonard Chi <leonardocompsci@gmail.com>
2025-10-04 14:43:33 +0100

    wip: Attemp for 2390

---
+++ python/2390-removing-stars-from-a-string.py
@@ -0,0 +1,15 @@
+class Solution251004:  # ! WRONG and not clean
+    def removeStars(self, s: str) -> str:
+        i = -1
+        result = []
+        star = 0
+        while i >= -len(s):
+            if s[i] == "*":
+                star += 1
+            else:
+                i -= star
+                result.append(s[i])
+                star = 0
+            i -= 1
+        result.reverse()
+        return "".join(result)
