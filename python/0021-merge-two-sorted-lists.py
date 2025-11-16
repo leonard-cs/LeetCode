284abdae4215c527e4854e233274568eb7ba38e3
Leonard Chi <leonardocompsci@gmail.com>
2025-11-16 13:08:20 +0000

    feat: 21E add cleaner code (NeetCode150)

--- python/0021-merge-two-sorted-lists.py
+++ python/0021-merge-two-sorted-lists.py
@@ -40,3 +40,23 @@ class Solution251116:
             tail.next = list2
 
         return result
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = node = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+
+        node.next = list1 or list2
+
+        return dummy.next
