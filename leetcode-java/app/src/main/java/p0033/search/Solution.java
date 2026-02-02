3cca992fcf2a610dabf4312a8d2f24eba2db3a16
Leonard Chi <leonardocompsci@gmail.com>
2026-02-02 20:10:28 +0000

    java: solve 33M

---
+++ leetcode-java/app/src/main/java/p0033/search/Solution.java
@@ -0,0 +1,36 @@
+package p0033.search;
+
+class Solution {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {  // Left half sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {  // Right half sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,1,3};
+        System.out.println(search(nums, 3));
+    }
+}
